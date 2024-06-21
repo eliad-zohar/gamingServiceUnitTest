4dca4195ca0f7282ea1c89c12ea04b2481f03430
Eliad Zohar <eliad@papa.digital>
2024-06-21 12:19:00 +0300

    2 rollbacks

--- playGround.js
+++ playGround.js
@@ -18,6 +18,10 @@ async function runBetTestFlow() {
     const commit2 = await commitBet(bet2.id);
 
     const rollback = await rollbackBet(10, bet.id, win.id, bet.betslipId);
+
+    const win2 = await winBet(10, bet.id, bet.transactionId, bet.betslipId);
+
+    const rollback2 = await rollbackBet(10, bet.id, win2.id, bet.betslipId);
   
 
   } catch (error) {
